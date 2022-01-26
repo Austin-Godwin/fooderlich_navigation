ef0c2f53b69899cebfd7e37c09a885e90fc9f28f
Austin Godwin <austingodwin18@gmail.com>
2022-01-26 07:28:15 +0100

    App State Manager Configuration Started

--- lib/models/app_state_manager.dart
+++ lib/models/app_state_manager.dart
@@ -1,2 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+
+class FooderlichTab {
+  static const int explore = 0;
+  static const int recipe = 1;
+  static const int toBuy = 2;
+}
+
+class AppStateManager extends ChangeNotifier {
+
+}
