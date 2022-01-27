d5ab307cfc8e752277d5ec1906f51bc7b3d2016a
Austin Godwin <austingodwin18@gmail.com>
2022-01-27 06:17:26 +0100

    App Router Added

---
+++ lib/navigation/app_router.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../models/models.dart';
+import '../screens/screens.dart';
+
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+  final AppStateManager appStateManager;
+  final GroceryManager groceryManager;
+  final ProfileManager profileManager;
+
+  AppRouter({
+    required this.appStateManager,
+    required this.groceryManager,
+    required this.profileManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+    appStateManager.addListener(notifyListeners);
+    groceryManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+  }
+
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      onPopPage: _handlePopPage,
+      pages: [],
+    );
+  }
+
+  bool _handlePopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+}
