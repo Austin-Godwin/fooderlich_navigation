d6541f2407014b49f624c141dc9400c01fbbf2ce
Austin Godwin <austingodwin18@gmail.com>
2022-01-30 21:27:06 +0100

    Altered

--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -37,9 +37,9 @@ class EmptyGroceryScreen extends StatelessWidget {
               ),
               color: Colors.green,
               onPressed: () {
-                // TODO: Update user's selected tab
-                Provider.of<AppStateManager>(context, listen: false)
-                    .goToRecipe();
+                // // TODO: Update user's selected tab
+                // Provider.of<AppStateManager>(context, listen: false)
+                //     .goToRecipe();
               },
             ),
           ],
