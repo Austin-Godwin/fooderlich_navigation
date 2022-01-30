63a39b1df5e50fe78dc75576b8bb29970331c80d
Austin Godwin <austingodwin18@gmail.com>
2022-01-30 21:26:26 +0100

    Restored

--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -24,11 +24,11 @@ class EmptyGroceryScreen extends StatelessWidget {
               style: Theme.of(context).textTheme.headline6,
             ),
             const SizedBox(height: 16.0),
-            // const Text(
-            //   'Shopping for ingredients?\n'
-            //   'Tap the + button to write them down!',
-            //   textAlign: TextAlign.center,
-            // ),
+            const Text(
+              'Shopping for ingredients?\n'
+              'Tap the + button to write them down!',
+              textAlign: TextAlign.center,
+            ),
             MaterialButton(
               textColor: Colors.white,
               child: const Text('Browse Recipes'),
